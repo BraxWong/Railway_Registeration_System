cff84c08a5fe7be0940c4488f711b1ce9991f6c3
BraxWong <ghostenjob@gmail.com>
2022-08-10 17:19:08 +0800

    Train Registeration System (Version 1.0.0) is LIVE.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,3 @@
-// 1. Administrator mode: There are two modes in this project – the administrator mode and the user mode. For the first time you have to create Users from the Admin mode. Once users are created, you can use the same user again and again, and they will be stored in separate files.
-
-// The operations related to both these modes are quite similar in this railway reservation system project in C++. In Admin mode, you can create detail database, add details, display details, perform user management functions and display passenger details.
-
-// 2. User mode: As aforementioned, at first you need to go to the administrator mode and create users. After that, you can use these users as you wish, and the information related to them will be recorded in separate files. In the user mode, you can create id database, add details and display details.
-
-// 3. Train and Reservation details: In this railway reservation system project in C++, users can get both the train details and the train reservation details. The details to be provided for train are train no., train name, boarding point, destination point, no. of seats in first class and fare per ticket, no. of seats in second class and fare per ticket and date of travel.
-
-// And, in case of reservation details, the information to be provided are train no., train name, boarding point, destination point, no. of seats required, seat class specification, date of reservation, passenger category and amount to be paid. According to the no. of seats, additional information – passenger name and passenger age are to be provided. The passenger category is divided as: military, senior citizen, children and none.
-
-// 4. Cancel reservation: It is somewhat similar to the feature mentioned above. This feature requires the date of cancellation in this railway reservation system project in C++. Then cancellation details can be displayed; the details here include train no., train name, boarding point, destination point, passenger class, no. of seats to be cancelled, passenger name and age, date of cancellation and the amount to be collected back.
-
-// 5. Password: This railway reservation system project in C++ requires the administrator password to access the admin mode. The password is “codewithc”. If you enter the wrong password, it displays the message – “You are not permitted to login.”
 #include <limits>
 #include <iostream>
 #include <string>
@@ -46,13 +33,17 @@ void savingFile(string str, char* line, int *num){
     object.close();
 }
 
-string saveAnotherFile(string str){
+string saveAnotherFile(string str,int decision){
     ifstream object;
     object.open(str);
     string output,output2;
+    int count = 0;
     while(object.good()){
-        getline(object,output,',');
-        output2 = output2 + output;
+        getline(object,output,'\n');
+        if(count > 0 || decision == 1){
+            output2 = output2 + output; 
+        }
+       count++;
     }
     return output2;
 }
@@ -111,6 +102,8 @@ void Admin::CreatingAndTrainSchedule() {
     }
     trainSchedule.close();
 }
+
+
 //A private method that verifies the username
 bool Admin::verifyUsername(string fileName,string username, int *counter){
     //character arrays to store the data from the files 
@@ -293,96 +286,72 @@ void Admin::printDatabase(){
 }
 
 // //This admin method is for creating user and passenger info
+
 void Admin::userRegisteration() {
+    //Clearing inputs
     clearingInput();
-    string input = saveAnotherFile("User_Info.csv");
-    ofstream fileName = createFiles("User_Info.csv");
-    string username, password,passenger_Name,age,city;
-    fileName << "Username" << "," << "Password" << "," << "Passenger_Name" << "," << "Age" << "," << "City" << "," << "Upcoming_Trip" << endl;
+    //for spacing at the beginning of the files
+    int num = 0;
+    string username, password, passengerName, age, city, username_file = "Username.txt", password_file = "Password.txt", Passenger_Name_File = "Name.txt", Age_file = "Age.txt", City_File = "City.txt";
+    //To store pre-existing data within the files
+    char username_line[COL_WIDTH + 1],password_line[COL_WIDTH+1], passenger_line[COL_WIDTH + 1], name_line[COL_WIDTH + 1], age_line[COL_WIDTH + 1], city_line[COL_WIDTH + 1];
+    //while loop to check whether the username and passwords are 8 characters or longer
     while(true){
-        cout << "Please enter your username (8 characters or longer):";
+        cout << "Please enter your username(8 characters or longer):";
         getline(cin,username);
-        cout << "Please enter your password (8 characters or longer):";
+        cout << "Please enter your password(8 characters or longer):";
         getline(cin,password);
         if(string_leng(username) && string_leng(password)){
             break;
         }
+        cout << "Username and password has to be 8 characters or longer.\n";
     }
-    cout << "Please enter your name:";
-    getline(cin,passenger_Name);
+    //Asking for name, age, and city
+    cout << "Please enter your first name:";
+    getline(cin,passengerName);
     cout << "Please enter your age:";
     getline(cin,age);
     cout << "Please enter the city where you reside:";
     getline(cin,city);
-    fileName << input;
-    fileName << username << "," << password << "," << passenger_Name << "," << age << "," << city << "," << "NULL";
-    fileName.close();
-    
+    //Saving all the files and putting the data in the character arrays 
+    savingFile(username_file,username_line,&num);
+    savingFile(password_file,password_line,&num);
+    savingFile(Passenger_Name_File,passenger_line,&num);
+    savingFile(Age_file,age_line,&num);
+    savingFile(City_File,city_line,&num);
+    //Wiping the files and creating ofstream objects
+    ofstream fUser(username_file), fPassword(password_file), fName(Passenger_Name_File),fAge(Age_file),fCity(City_File);
+    if(!fUser || !fPassword || !fName || !fAge || !fCity){
+        cout << "The file does not exist.Exiting\n";
+        return;
+    }
+    //if num is not zero, that means those files do not exist, therefore these lines are not required. Else they will paste all the pre-existing data into the corresponding files
+    if(num == 0){
+        fUser << username_line << " ";
+        fPassword << password_line << " ";
+        fName << passenger_line << " ";
+        fAge << age_line << " ";
+        fCity << city_line << " ";
+    }
+    //Putting all the data into the corresponding files
+    fUser << username;
+    fPassword << password;
+    fName << passengerName;
+    fAge << age;
+    fCity << city;
+    //Closing the files
+    fUser.close();
+    fPassword.close();
+    fName.close();
+    fAge.close();
+    fCity.close();
 }
-// void Admin::userRegisteration() {
-//     //Clearing inputs
-//     clearingInput();
-//     //for spacing at the beginning of the files
-//     int num = 0;
-//     string username, password, passengerName, age, city, username_file = "Username.txt", password_file = "Password.txt", Passenger_Name_File = "Name.txt", Age_file = "Age.txt", City_File = "City.txt";
-//     //To store pre-existing data within the files
-//     char username_line[COL_WIDTH + 1],password_line[COL_WIDTH+1], passenger_line[COL_WIDTH + 1], name_line[COL_WIDTH + 1], age_line[COL_WIDTH + 1], city_line[COL_WIDTH + 1];
-//     //while loop to check whether the username and passwords are 8 characters or longer
-//     while(true){
-//         cout << "Please enter your username(8 characters or longer):";
-//         getline(cin,username);
-//         cout << "Please enter your password(8 characters or longer):";
-//         getline(cin,password);
-//         if(string_leng(username) && string_leng(password)){
-//             break;
-//         }
-//         cout << "Username and password has to be 8 characters or longer.\n";
-//     }
-//     //Asking for name, age, and city
-//     cout << "Please enter your first name:";
-//     getline(cin,passengerName);
-//     cout << "Please enter your age:";
-//     getline(cin,age);
-//     cout << "Please enter the city where you reside:";
-//     getline(cin,city);
-//     //Saving all the files and putting the data in the character arrays 
-//     savingFile(username_file,username_line,&num);
-//     savingFile(password_file,password_line,&num);
-//     savingFile(Passenger_Name_File,passenger_line,&num);
-//     savingFile(Age_file,age_line,&num);
-//     savingFile(City_File,city_line,&num);
-//     //Wiping the files and creating ofstream objects
-//     ofstream fUser(username_file), fPassword(password_file), fName(Passenger_Name_File),fAge(Age_file),fCity(City_File);
-//     if(!fUser || !fPassword || !fName || !fAge || !fCity){
-//         cout << "The file does not exist.Exiting\n";
-//         return;
-//     }
-//     //if num is not zero, that means those files do not exist, therefore these lines are not required. Else they will paste all the pre-existing data into the corresponding files
-//     if(num == 0){
-//         fUser << username_line << " ";
-//         fPassword << password_line << " ";
-//         fName << passenger_line << " ";
-//         fAge << age_line << " ";
-//         fCity << city_line << " ";
-//     }
-//     //Putting all the data into the corresponding files
-//     fUser << username;
-//     fPassword << password;
-//     fName << passengerName;
-//     fAge << age;
-//     fCity << city;
-//     //Closing the files
-//     fUser.close();
-//     fPassword.close();
-//     fName.close();
-//     fAge.close();
-//     fCity.close();
-// }
 //Since password is private, then a password getter is needed
 string Admin::getPassword() {
     return password;
 }
-//A Main meny class
+
+//A Main menu class
 class Menu {
   public:
     void showMenu();  
@@ -390,6 +359,10 @@ class Menu {
     void userMenu();
 };
 
+void Menu::userMenu() {
+    clearingInput();
+
+}
 //This is an Admin Menu
 void Menu::adminMenu() {
     clearingInput();
@@ -466,7 +439,7 @@ void Menu::showMenu() {
             break;
         }
         else if(decision == 2){
-            //userMenu();
+            userMenu();
             break;
         }
         else {
